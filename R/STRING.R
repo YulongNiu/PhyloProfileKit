106d7a35ef45b75fc844997457b629e4a9da7153
YulongNiu <niuylscu@gmail.com>
2017-01-21 18:43:52 +0800

    use index instead of top in PPSTRING

--- R/STRING.R
+++ R/STRING.R
@@ -7,7 +7,7 @@ NULL
 ##'
 ##' @title Constructor from the STRING database
 ##' @param speID Number. NCBI taxonomy identifier must be present in the STRING database. Use \code{get_STRING_species()} from the STRINGdb package to determine supported organisms.
-##' @param top Number or "all". Top number of proteins or the whole genome (set as "all")
+##' @param idx A numeric vector or "all". 
 ##' @param STRINGnorm Whether you use the normalized or raw bit scores.
 ##' @param n The number of CPUs or processors, and the default value is 1.
 ##' @param symbets Whether to keep only symmetrical best hits.
@@ -15,20 +15,21 @@ NULL
 ##' @return A \code{PP} object.
 ##' @examples
 ##' ## human top 5 proteins
-##' PPSTRING(9606, top = 5, n = 2)
+##' PPSTRING(9606, idx = 1:5, n = 2)
 ##'
 ##' \dontrun{
 ##' ## all human proteins
-##' PPRCSTRING(9606, top = "all", n = 4)
+##' PPRCSTRING(9606, idx = "all", n = 4)
 ##' }
 ##' @author Yulong Niu \email{niuylscu@@gmail.com}
 ##' @import STRINGdb
 ##' @importFrom doParallel registerDoParallel stopImplicitCluster
 ##' @importFrom foreach foreach %dopar%
 ##' @importFrom magrittr %>%
+##' @importFrom methods new
 ##' @export
 ##' 
-PPSTRING <- function(speID, top = 10, symbets = FALSE, STRINGnorm = FALSE, n = 1, verbose = FALSE) {
+PPSTRING <- function(speID, idx = 1:10, symbets = FALSE, STRINGnorm = FALSE, n = 1, verbose = FALSE) {
 
   ## register multiple core
   registerDoParallel(cores = n)
@@ -44,27 +45,26 @@ PPSTRING <- function(speID, top = 10, symbets = FALSE, STRINGnorm = FALSE, n = 1
   pIDsLen <- length(pIDs)
 
   ## determine protein number
-  if (is.numeric(top)) {
-    if (top < pIDsLen) {
-      tN <- top
-    } else {
-      tN <- pIDsLen
-    }
+  if (is.numeric(idx)) {
+    idx <- idx[idx <= pIDsLen]
+    if (length(idx) == 0) {
+      return(new('PP'))
+    } else {}
   } else {
     ## all proteins
-    tN <- pIDsLen
+    idx <- seq_len(pIDsLen)
   }
 
-  proMat <- foreach(i = 1:tN, .combine = rbind) %dopar% {
+  proMat <- foreach(i = 1:length(idx), .combine = rbind) %dopar% {
 
     if (verbose) {
-      cat(paste0('It is running ', i, ' in a total of ', tN, '.\n'))
+      cat(paste0('It is running ', i, ' in a total of ', length(idx), '.\n'))
     } else {}
 
     ## initvec
     eachInit <- rep(0, length(spIDs))
 
-    eachM <- sdb$get_homologs_besthits(pIDs[i], symbets = symbets, bitscore_threshold = 0)
+    eachM <- sdb$get_homologs_besthits(pIDs[idx[i]], symbets = symbets, bitscore_threshold = 0)
 
     if (nrow(eachM) != 0) {
 
@@ -82,7 +82,7 @@ PPSTRING <- function(speID, top = 10, symbets = FALSE, STRINGnorm = FALSE, n = 1
   }
 
   ## STRING IDs
-  rownames(proMat) <- pIDs[1:tN]
+  rownames(proMat) <- pIDs[idx]
   colnames(proMat) <- spIDs
 
   ## stop multiple core
