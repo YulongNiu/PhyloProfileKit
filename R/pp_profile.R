68ebfefb3293deec6a30a600c4e4647dbf9734b9
Yulong Niu <niuylscu@gmail.com>
2017-02-12 16:51:45 +0800

    add pp_profile

--- R/pp_profile.R
+++ R/pp_profile.R
@@ -1,15 +1,20 @@
 ##' Plot profiles
 ##'
-##' Plot phylogenetic profile
+##' Plot a phylogenetic profile, which is a matrix.
 ##'
 ##' @title pp plot tiles
-##' @param x A character vector
+##' @param x A numeric matrix.
 ##' @param ... Parameters passed to \code{geom_tile()} in the ggplot2 package.
 ##' @return A \code{gg} class object
 ##' @examples
-##' pp_tile(rep(letters[1:3], 4))
-##' pp_tile(rep(LETTERS[1:2], 3)) + scale_fill_manual(values = c('red','blue', 'green'))
-##' pp_tile(rep(letters[1:3], 4)) + coord_flip()
+##' require('magrittr')
+##'
+##' ## a binning profile
+##' ppB <- sample(0:1, 5 * 20, replace = TRUE) %>% matrix(nrow = 5)
+##' pp_profile(ppB) + scale_fill_manual(values = c('grey91', 'steelblue'))
+##' ## a continuous profile
+##' ppC <- rnorm(5 * 20) %>% matrix(nrow = 5)
+##' pp_profile(ppC) + scale_fill_gradient2()
 ##' @author Yulong Niu \email{niuylscu@@gmail.com}
 ##' @importFrom ggplot2 ggplot geom_tile labs scale_x_continuous scale_y_continuous scale_y_reverse aes_string
 ##' @seealso \code{\link[ggplot2]{geom_tile}}
@@ -17,4 +22,23 @@
 ##' 
 pp_profile <- function(x, ...) {
 
+  nr <- nrow(x)
+  nc <- ncol(x)
+  if (isBinMat_internal(x)) {
+    x <- factor(c(x))
+  } else {
+    x <- c(x)
+  }
+  m <- data.frame(x = rep(1:nc, each = nr),
+                  y = rep(1:nr, nc),
+                  fill = x)
+
+  pObj <- ggplot(m, aes_string('x', 'y')) +
+    geom_tile(aes_string(fill = 'fill'), ...) +
+    labs(x = NULL, y = NULL) +
+    scale_y_continuous(expand = c(0, 0), breaks = NULL) +
+    scale_x_continuous(expand = c(0, 0), breaks = NULL) +
+    theme_pp(legend.position='none')
+
+  return(pObj)
 }
