d5c6f5ebc1181aa2f1b72cd14690c2030c79c983
Yulong Niu <niuylscu@gmail.com>
2017-02-12 14:49:12 +0800

    use coord_flip() instead

--- R/pp_text.R
+++ R/pp_text.R
@@ -1,26 +1,22 @@
 ##' Plot text
 ##'
 ##' Plot text, for example column names and row names.
-##'
-##' \code{pp_htext()}: Plot text in the horizontal direction.
-##'
-##' \code{pp_vtext()}: Plot text in the vertical direction.
-##'
+##' 
 ##' @title pp plot text
 ##' @param x A character vector
 ##' @param shift A numeric value indicating shit scale.
 ##' @param ... Parameters passed to \code{geom_text()} in the ggplot2 package.
 ##' @return A \code{gg} class object
 ##' @examples
-##' pp_htext(letters[1:10], colour = 'grey55', size = 3)
-##' pp_vtext(rep(LETTERS[1:2], 3), colour = factor(rep(c('blue', 'red'), 3)))
+##' pp_text(letters[1:10], colour = 'grey55', size = 3)
+##' pp_text(rep(LETTERS[1:2], 3), colour = factor(rep(c('blue', 'red'), 3)))
+##' pp_text(letters[1:5]) + coord_flip()
 ##' @author Yulong Niu \email{niuylscu@@gmail.com}
 ##' @importFrom ggplot2 ggplot geom_text labs scale_x_continuous scale_y_continuous scale_y_reverse aes_string
-##' @rdname pp_text
 ##' @seealso \code{\link[ggplot2]{geom_text}}
 ##' @export
 ##' 
-pp_htext <- function(x, shift = 0.5, ...) {
+pp_text <- function(x, shift = 0.5, ...) {
 
   xlen <- length(x)
   m <- data.frame(x = rep(0, xlen),
@@ -36,25 +32,3 @@ pp_htext <- function(x, shift = 0.5, ...) {
 
   return(tObj)
 }
-
-
-##' @inheritParams pp_htext
-##' @rdname pp_text
-##' @export
-##' 
-pp_vtext <- function(x, shift = 0.5, ...) {
-  xlen <- length(x)
-  m <- data.frame(y = rep(0, xlen),
-                  x = seq(shift, xlen - shift, 1),
-                  label = x)
-
-  tObj <- ggplot(m, aes_string('x', 'y')) +
-    geom_text(aes_string(label = 'label'), ...) +
-    labs(x = NULL, y = NULL) +
-    scale_y_continuous(expand = c(0, 0), breaks = NULL) +
-    scale_x_continuous(expand = c(0, 0), limits = c(0, xlen), breaks = NULL) +
-    theme_pp(legend.position='none')
-
-  return(tObj)
-}
-
--- R/pp_tile.R
+++ R/pp_tile.R
@@ -2,50 +2,24 @@
 ##'
 ##' Plot tiles to discrete groups
 ##'
-##' \code{pp_htext()}: Plot tiles in the horizontal direction.
-##'
-##' \code{pp_vtext()}: Plot tiles in the vertical direction.
-##'
 ##' @title pp plot tiles
 ##' @param x A character vector
 ##' @param ... Parameters passed to \code{geom_tile()} in the ggplot2 package.
 ##' @return A \code{gg} class object
 ##' @examples
-##' pp_htile(rep(letters[1:3], 4))
-##' pp_vtile(rep(LETTERS[1:2], 3)) + scale_fill_manual(values = c('red','blue', 'green'))
+##' pp_tile(rep(letters[1:3], 4))
+##' pp_tile(rep(LETTERS[1:2], 3)) + scale_fill_manual(values = c('red','blue', 'green'))
+##' pp_tile(rep(letters[1:3], 4)) + coord_flip()
 ##' @author Yulong Niu \email{niuylscu@@gmail.com}
 ##' @importFrom ggplot2 ggplot geom_tile labs scale_x_continuous scale_y_continuous scale_y_reverse aes_string
-##' @rdname pp_tile
 ##' @seealso \code{\link[ggplot2]{geom_tile}}
 ##' @export
 ##' 
-pp_htile <- function(x, ...) {
+pp_tile <- function(x, ...) {
 
   xlen <- length(x)
   m <- data.frame(x = rep(0, xlen),
                   y = 1:xlen,
-                  fill = x)
-
-  tObj <- ggplot(m, aes_string('x', 'y')) +
-    geom_tile(aes_string(fill = 'fill'), ...) +
-    labs(x = NULL, y = NULL) +
-    scale_y_continuous(expand = c(0, 0), breaks = NULL) +
-    scale_x_continuous(expand = c(0, 0), breaks = NULL) +
-    theme_pp(legend.position='none')
-
-  return(tObj)
-}
-
-
-##' @inheritParams pp_vtile
-##' @rdname pp_tile
-##' @export
-##' 
-pp_vtile <- function(x, ...) {
-
-  xlen <- length(x)
-  m <- data.frame(y = rep(0, xlen),
-                  x = 1:xlen,
                   fill = x)
 
   tObj <- ggplot(m, aes_string('x', 'y')) +
